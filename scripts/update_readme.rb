611048d1dd4f6459a480e3c3e325344cad8c42e6
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-12-05 21:56:33 +0100

    Update update_readme.rb

--- scripts/update_readme.rb
+++ scripts/update_readme.rb
@@ -3,6 +3,7 @@ require 'dotenv/load'
 require 'json'
 require_relative 'fetch_activity'
 require_relative 'fetch_repos'
+require_relative 'fetch_followers'
 
 # Load environment variables from .env file
 GITHUB_TOKEN = ENV['TOKEN']
@@ -11,9 +12,10 @@ USERNAME = ENV['USERNAME']
 # Load translations from JSON file
 TRANSLATIONS = JSON.parse(File.read(File.join(__dir__, 'translations.json')), symbolize_names: true)
 
+# Initialize Octokit client
 client = Octokit::Client.new(access_token: GITHUB_TOKEN)
 
-# Fetch recent activities and top repositories
+# Fetch data (recent activity, top repos and followers)
 english_activities = fetch_recent_activities(client, USERNAME, :en, TRANSLATIONS)
 italian_activities = fetch_recent_activities(client, USERNAME, :it, TRANSLATIONS)
 spanish_activities = fetch_recent_activities(client, USERNAME, :es, TRANSLATIONS)
@@ -22,13 +24,16 @@ english_repos = fetch_top_repositories(client, USERNAME, :en, TRANSLATIONS)
 italian_repos = fetch_top_repositories(client, USERNAME, :it, TRANSLATIONS)
 spanish_repos = fetch_top_repositories(client, USERNAME, :es, TRANSLATIONS)
 
-# Read the current README.md and update its content
+followers_table = generate_followers_table(client, USERNAME)
+
+# Update README content
 def update_readme(filename, activities, top_repos, language, translations)
   readme = File.read(filename)
   activity_header = translations[language][:recent_activity]
   repo_header = translations[language][:repo_name]
   stars_header = translations[language][:stars]
   forks_header = translations[language][:forks]
+  followers_header = translations[language][:followers]
   
   new_content = readme.gsub(/<!--START_SECTION:activity-->.*<!--END_SECTION:activity-->/m) do
     "<!--START_SECTION:activity-->\n| #{activity_header} |\n| --- |\n#{activities.join("\n")}\n<!--END_SECTION:activity-->"
@@ -38,10 +43,14 @@ def update_readme(filename, activities, top_repos, language, translations)
     "<!--START_SECTION:top_repos-->\n| #{repo_header} | #{stars_header} | #{forks_header} |\n| --- | --- | --- |\n#{top_repos.join("\n")}\n<!--END_SECTION:top_repos-->"
   end
 
+  new_content.gsub!(/<!--START_SECTION:followers-->.*<!--END_SECTION:followers-->/m) do
+    "<!--START_SECTION:followers-->\n#{followers}\n<!--END_SECTION:followers-->"
+  end  
+
   File.open(filename, 'w') { |file| file.write(new_content) }
 end
 
 # Update each README file
-update_readme(File.join(__dir__, '..', 'README.md'), english_activities, english_repos, :en, TRANSLATIONS)
-update_readme(File.join(__dir__, '..', 'README.it.md'), italian_activities, italian_repos, :it, TRANSLATIONS)
-update_readme(File.join(__dir__, '..', 'README.es.md'), spanish_activities, spanish_repos, :es, TRANSLATIONS)
+update_readme(File.join(__dir__, '..', 'README.md'), english_activities, english_repos, followers_table, :en, TRANSLATIONS)
+update_readme(File.join(__dir__, '..', 'README.it.md'), italian_activities, italian_repos, followers_table, :it, TRANSLATIONS)
+update_readme(File.join(__dir__, '..', 'README.es.md'), spanish_activities, spanish_repos, followers_table, :es, TRANSLATIONS)
